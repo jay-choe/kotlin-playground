bb7d2faf875718070ae6452b4b0cf548d303190b
jay-choe <jay0764@naver.com>
2024-03-10 16:40:22 +0900

    init

---
+++ src/test/kotlin/com/jay/playground/PlaygroundApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jay.playground
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PlaygroundApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
