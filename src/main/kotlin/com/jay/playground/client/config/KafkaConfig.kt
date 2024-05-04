e0b8496e8fbbb251a40d99b6cdee2802100491bc
jay-choe <jay0764@naver.com>
2024-05-05 03:14:16 +0900

    카프카 consumer 설정 및 factory 추가

--- src/main/kotlin/com/jay/playground/client/config/KafkaConfig.kt
+++ src/main/kotlin/com/jay/playground/client/config/KafkaConfig.kt
@@ -1,10 +1,13 @@
 package com.jay.playground.client.config
 
 import com.jay.playground.client.dto.PaymentDone
+import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.producer.ProducerConfig
 import org.apache.kafka.common.serialization.StringSerializer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory
 import org.springframework.kafka.core.DefaultKafkaProducerFactory
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.kafka.support.serializer.JsonSerializer
@@ -12,18 +15,39 @@ import org.springframework.kafka.support.serializer.JsonSerializer
 @Configuration
 class KafkaConfig {
 
+    private val bootStrapServerLocation = "localhost:9092"
     fun producerConfig(): Map<String, Any> {
-        return mutableMapOf<String, Any>().also {
-            it[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = "localhost:9092"
-            it[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java.name
-            it[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = JsonSerializer::class.java.name
-            it[ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG] = true
+        return mutableMapOf<String, Any>().apply {
+            this[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = bootStrapServerLocation
+            this[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java.name
+            this[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = JsonSerializer::class.java.name
+            this[ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG] = true
         }
 
+    }
+
+    fun consumerConfig(): Map<String, Any> {
+        return mutableMapOf<String, Any>().apply {
+            this[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = bootStrapServerLocation
+            this[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringSerializer::class.java.name
+            this[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = JsonSerializer::class.java.name
+            this[ConsumerConfig.GROUP_ID_CONFIG] = "test-group"
+            this[ConsumerConfig.AUTO_OFFSET_RESET_CONFIG] = "earliest"
+            this[ConsumerConfig.ALLOW_AUTO_CREATE_TOPICS_CONFIG] = true
+        }
     }
 
     @Bean
     fun kafkaProducer(): KafkaTemplate<String, PaymentDone> {
         return KafkaTemplate(DefaultKafkaProducerFactory(producerConfig()))
+    }
+
+    @Bean
+    fun kafkaConsumer(): ConcurrentKafkaListenerContainerFactory<String, PaymentDone> {
+        val factory = ConcurrentKafkaListenerContainerFactory<String, PaymentDone>()
+        factory.consumerFactory = DefaultKafkaConsumerFactory(consumerConfig())
+        factory.setConcurrency(3)
+
+        return factory
     }
 }
