f5ca7234c387512553db343e19f07a7ef2806b09
jay-choe <jay0764@naver.com>
2024-05-15 20:41:17 +0900

    Add dlt config
    - 모든 재시도 처리 실패시 DLT에 해당 메시지 남겨두도록 설정 변경

--- src/main/kotlin/com/jay/playground/listener/TestConsumerService.kt
+++ src/main/kotlin/com/jay/playground/listener/TestConsumerService.kt
@@ -3,6 +3,7 @@ package com.jay.playground.listener
 import com.jay.playground.client.dto.PaymentDone
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.annotation.RetryableTopic
+import org.springframework.kafka.retrytopic.DltStrategy
 import org.springframework.messaging.handler.annotation.Payload
 import org.springframework.retry.annotation.Backoff
 import org.springframework.stereotype.Service
@@ -15,14 +16,23 @@ class TestConsumerService {
 
     val atomicInteger = AtomicInteger()
 
-//    @KafkaListener(topics = ["Topic2"], groupId = "test-group", containerFactory = "paymentDoneFactory")
+    //    @KafkaListener(topics = ["Topic2"], groupId = "test-group", containerFactory = "paymentDoneFactory")
     fun listen(paymentDoneEvent: PaymentDone) {
         println("Hi : ${atomicInteger.addAndGet(1)}")
         throw RuntimeException("예외 던지기")
     }
 
-    @KafkaListener(topics = ["Topic2"], groupId = "test-group", containerFactory = "paymentDoneFactory")
-    @RetryableTopic(backoff = Backoff(value = 3000L), attempts = "3", autoCreateTopics = "true")
+    @KafkaListener(
+        topics = ["Topic2"],
+        groupId = "test-group",
+        containerFactory = "paymentDoneFactory"
+    )
+    @RetryableTopic(
+        backoff = Backoff(value = 3000L),
+        attempts = "3",
+        autoCreateTopics = "true",
+        dltStrategy = DltStrategy.FAIL_ON_ERROR
+    )
     fun listenWithAsyncRetry(paymentDoneEvent: PaymentDone) {
         println("Hi : ${atomicInteger.addAndGet(1)}")
         throw RuntimeException("예외 던지기")
