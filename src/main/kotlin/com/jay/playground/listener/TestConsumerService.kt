f410ef8a94d88c715f0096e0a29e036bc74d2624
jay-choe <jay0764@naver.com>
2024-05-15 20:32:13 +0900

    Add Async Retry
    - 지정 횟수 만큼 retry
    
    - retry-{재시도 횟수} 토픽으로 보내면서 재시도 처리

--- src/main/kotlin/com/jay/playground/listener/TestConsumerService.kt
+++ src/main/kotlin/com/jay/playground/listener/TestConsumerService.kt
@@ -2,14 +2,29 @@ package com.jay.playground.listener
 
 import com.jay.playground.client.dto.PaymentDone
 import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.kafka.annotation.RetryableTopic
 import org.springframework.messaging.handler.annotation.Payload
+import org.springframework.retry.annotation.Backoff
 import org.springframework.stereotype.Service
+import java.lang.RuntimeException
+import java.time.Duration
+import java.util.concurrent.atomic.AtomicInteger
 
 @Service
 class TestConsumerService {
 
+    val atomicInteger = AtomicInteger()
+
 //    @KafkaListener(topics = ["Topic2"], groupId = "test-group", containerFactory = "paymentDoneFactory")
     fun listen(paymentDoneEvent: PaymentDone) {
-        println(paymentDoneEvent.idempotentKey)
+        println("Hi : ${atomicInteger.addAndGet(1)}")
+        throw RuntimeException("예외 던지기")
+    }
+
+    @KafkaListener(topics = ["Topic2"], groupId = "test-group", containerFactory = "paymentDoneFactory")
+    @RetryableTopic(backoff = Backoff(value = 3000L), attempts = "3", autoCreateTopics = "true")
+    fun listenWithAsyncRetry(paymentDoneEvent: PaymentDone) {
+        println("Hi : ${atomicInteger.addAndGet(1)}")
+        throw RuntimeException("예외 던지기")
     }
 }
